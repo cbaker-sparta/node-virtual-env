e283088dca9516009dd2ba24572efb2ebf039855
Tech-A24 <tech-a24@Tech-A24s-MacBook-Air.local>
2019-05-14 11:58:02 +0100

    potential solution

--- environment/provision.sh
+++ environment/provision.sh
@@ -5,8 +5,10 @@ sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
-npm install -g
 npm install pm2 -g
 sudo systemctl start nginx
 cd /app
+npm install express
+npm install mongoose
+npm install ejs
 pm2 start app.js
